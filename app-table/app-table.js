d4b4cef88eef4b53805e6d89ed357e07ee71f28d
Serik <serik.shaikamalov@gmail.com>
2024-05-24 23:21:33 +0500

    apply default css for app-table & an example how to use app-table

--- app-table/app-table.js
+++ app-table/app-table.js
@@ -1,13 +1,5 @@
 import { html, css, LitElement } from "https://cdn.jsdelivr.net/npm/lit@2/+esm";
 
-/**
- Find value in an object by key
- let item = {
-    work: {
-        id: 1
-    }
- }
- */
 const get = (item, keyArrayAsString) => {
   if (!keyArrayAsString) return;
   const keys = keyArrayAsString.split(".");
@@ -22,26 +14,20 @@ const get = (item, keyArrayAsString) => {
 
 export class AppTable extends LitElement {
   static styles = [
-    css`
-      :host {
-        display: block;
-      }
-      .app-table {
+    css`    
+      .app-table {        
         border-collapse: collapse;
         border-spacing: 0;
         width: 100%;
         border: 1px solid #ddd;
+        background-color: var(--background-color, rgb(229, 240, 252))
       }
       th,
       td {
-        border: none;
+        border-top: 1px solid rgb(222, 226, 230);
+        line-height: 1.5;
         text-align: left;
         padding: 8px;
-      }
-
-      tr:nth-child(even) {
-        background-color: #f2f2f2;
-        color: black;
       }
 
       thead > tr {
@@ -56,26 +42,20 @@ export class AppTable extends LitElement {
     `,
   ];
 
-  createRenderRoot() {
-    return this
-  }
-
   static get properties() {
     return {
+      // inputs
       columnConfig: { type: Array },
       data: { type: Array },
       hideColumnsOnMobile: { type: Array },
     };
   }
-
   constructor() {
     super();
-
     this.columnConfig = [];
     this.data = [];
     this.hideColumnsOnMobile = [];
   }
-
   connectedCallback() {
     super.connectedCallback();
   }
@@ -90,34 +70,34 @@ export class AppTable extends LitElement {
         <thead>
           <tr>
             ${this.columnConfig.map(({ label, key }) => {
-              return html`<th
+      return html`<th
                 class=${this.hideColumnsOnMobile.includes(key)
-                  ? "no-mobile"
-                  : ""}
+          ? "no-mobile"
+          : ""}
               >
                 ${label}
               </th>`;
-            })}
+    })}
           </tr>
         </thead>
         <tbody>
-          ${this.data.map((item) => {
-            return html`<tr>
+          ${this.data.map((item, index) => {
+      return html`<tr>
               ${this.columnConfig.map(({ key, label, formatter, render }) => {
-                return html`<td
+        return html`<td
                   class=${this.hideColumnsOnMobile.includes(key)
-                    ? "no-mobile"
-                    : ""}
+            ? "no-mobile"
+            : ""}
                 >
                   ${render
-                    ? render(item)
-                    : typeof formatter === "function"
-                    ? formatter(get(item, key))
-                    : get(item, key)}
+            ? render(item, index)
+            : typeof formatter === "function"
+              ? formatter(get(item, key))
+              : get(item, key)}
                 </td>`;
-              })}
+      })}
             </tr>`;
-          })}
+    })}
         </tbody>
       </table>
     `;
---
+++ examples/user-list.js
@@ -0,0 +1,72 @@
+import {
+    html,
+    LitElement,
+    css
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
+import '../app-table/app-table.js'
+
+export class UsersList extends LitElement {
+
+    static get properties() {
+        return {
+            columnConfig: { type: Object },
+            data: { type: Object }
+        };
+    }
+
+    static styles = [
+        css`      
+        `
+    ]
+
+    constructor() {
+        super()
+        this.columnConfig = [
+            {
+                key: "email",
+                label: "Email",
+            },
+            {
+                key: "name",
+                label: "Name",
+            },
+            {
+                key: "role",
+                label: "Role",
+            },
+            {
+                key: "createdAt",
+                label: "Created",
+                formatter: (v) => new Date(v).toLocaleString()
+            }
+        ]
+
+        this.data = [
+            {
+                email: "serik.shaikamalov@gmail.com",
+                name: "Serik",
+                role: "Admin",
+                createdAt: new Date("1991-05-26")
+            },
+            {
+                email: "arailym@gmail.com",
+                name: "Arailym",
+                role: "Editor",
+                createdAt: new Date("1991-05-26")
+            },
+            {
+                email: "ostin@mail.com",
+                name: "Ostin",
+                role: "Moderator",
+                createdAt: new Date("1991-05-26")
+            }
+        ]
+    }
+
+    render() {
+        return html`<h2>
+        <app-table .columnConfig=${this.columnConfig} .data=${this.data} style=${"--background-color: var(--md-sys-color-surface)"}></app-table>        
+      </h2>`;
+    }
+}
+customElements.define("user-list", UsersList);
